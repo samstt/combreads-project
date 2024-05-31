da74871f640d53bd6a052bebff3c2ed47a183a82
samstt <lixiaona99@gmail.com>
2024-05-31 23:57:32 +0800

    fixed bug for log in and log out feature

--- combreads/users/views.py
+++ combreads/users/views.py
@@ -18,10 +18,10 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request,"users/login.html"), {
+            return render(request,"users/login.html", {
                 "message": "Invalid Credentials"
-            }
-
+            })
+        
     return render(request,"users/login.html")
 
 def logout_view(request):

fb0a2708293efb59aae1c37f961a669d399e2215
samstt <lixiaona99@gmail.com>
2024-06-01 00:32:16 +0800

    included log in and log out features

--- combreads/users/views.py
+++ combreads/users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 
+
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:

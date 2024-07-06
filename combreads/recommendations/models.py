b2866e7a0d82bce60f720fc519c4e2932b4e5eba
samstt <lixiaona99@gmail.com>
2024-07-06 14:42:57 +0800

    set up static files

--- combreads/recommendations/models.py
+++ combreads/recommendations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class DailyReco(models.Model):
+    name = models.TextField()
+    location = models.TextField()
+    date = models.DateField(unique=True)
+
+    def __str__(self):
+        return f'Text for {self.date}'
--- combreads/recommendations/views.py
+++ combreads/recommendations/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import DailyReco
+from django.utils import timezone
 
 def suggestions(request):
-    return render(request, 'recommendations/recopage.html')
+    today = timezone.now().date()
+    daily_reco = DailyReco.objects.filter(date=today).first()
+    return render(request, 'recommendations/recopage.html', {'daily_reco': daily_reco})
